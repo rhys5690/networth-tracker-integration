e61bc0fc53c7f71125e43c4a49d6a3320d70ad97
rhys5690 <rhys5690@gmail.com>
2018-06-17 16:35:28 +1000

    set up mechanize

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -32,46 +32,34 @@ class AccountsController < ApplicationController
   def show
     if Account.where(user_id: current_user.id).first
       @account = Account.where(user_id: current_user.id).first
-      values_to_input = ['72806279', 5690, 'rhys5690']
+      values_to_input = ['72806279', '5690', 'rhys5690']
       a = Mechanize.new
       a.follow_meta_refresh = true
       a.redirect_ok = true
-      a.get('https://ibanking.stgeorge.com.au/ibank/loginPage.action') do |page|
-        my_page = page.form_with(action: 'logonActionSimple.action') do |f|
-          used_fields = f.fields.select do |field|
-            case field.type
-            when 'text'
-              true
-            when 'password'
-              true
-            else
-              false
-            end
-          end
-          used_fields[0].value = values_to_input[0]
-          used_fields[1].value = values_to_input[1]
-          used_fields[2].value = values_to_input[2]
-        end.click_button
-        # example submitting log in form
-        # my_page = page.form_with(action: '/account/login.php') do |f|
-        #   f.form_loginname  = ARGV[0]
-        #   f.form_pw         = ARGV[1]
-        # end.click_button
-        # example next action after submit
-        # my_page.links.each do |link|
-        #   text = link.text.strip
-        #   next if text.empty?
-        #   puts text
-        # end
-      end
+      page = a.get('https://ibanking.stgeorge.com.au/ibank/loginPage.action')
 
-      # delete if possible to do in merchanize
-      #  doc = HTTParty.get("https://ibanking.stgeorge.com.au/ibank/loginPage.action")
-      #  @parse_page ||= Nokogiri::HTML(doc)
-      #  @access_number = @parse_page.css("#access-number")
-      #  @security_number = @parse_page.css("#securityNumber")
-      #  @internet_password = @parse_page.css("#internet-password")
-      #  @parse_page.css("#internet-password")[0].attributes["id"].value
+      pp  page.form_with(action: 'logonActionSimple.action')
+      form = page.form_with(action: 'logonActionSimple.action') do |f|
+        used_fields = f.fields.select do |field|
+          case field.name
+          when 'userId'
+            true
+          when 'securityNumber'
+            true
+          when 'password'
+            true
+          else
+            false
+          end
+        end
+
+        used_fields[0].value = values_to_input[0]
+        used_fields[1].value = values_to_input[1]
+        used_fields[2].value = values_to_input[2]
+      end
+      button = form.button_with(:value => "Logon")
+      a.submit(form, button)
+
       @networth_total = 35_000
       @account_exists = true
     else
